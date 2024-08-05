4449e253de68ceae4b8d04c2cef3a30221a99ea8
AmourAnurag <anurag875636@gmail.com>
2024-08-05 16:18:03 +0530

    pRIME NUMBER

---
+++ PrimeNumber.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number ");
+        int num = sc.nextInt();
+        if (num < 2) {
+            System.out.println(num + " is not Prime");
+            return;
+        }
+        boolean isPrime = true;
+        int dev = 2;
+        int limit = (int) Math.sqrt(num);
+
+        while (dev <= limit) {
+            if (num % dev == 0) {
+                isPrime = false;
+                break;
+            }
+            dev++;
+        }
+        if (isPrime) {
+            System.out.println(num + " is Prime");
+        } else {
+            System.out.println(num + " is not Prime");
+
+        }
+
+    }
+
+    // TIME COMPLEXITY OF THIS CODE IS O(ROOT N)
+}
