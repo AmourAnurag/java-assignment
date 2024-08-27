1ddfa489957c2f10aa30de8cc1ae251e5736813c
AmourAnurag <anurag875636@gmail.com>
2024-08-27 11:06:13 +0530

    Q19 completed

--- Q19.java
+++ Q19.java
@@ -1,10 +1,16 @@
 public class Q19 {
-    public static void main(String args){
-        if (args.length()==0) {
-            System.out.println("No argument is provided");
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("No arguments provided. Please pass some values as command line arguments.");
+            return;
         }
-        for(int i=0;i<args.length();i+=2){
-           System.out.println(args[i]);
+
+        System.out.println("Alternate elements:");
+
+        for (int i = 0; i < args.length; i += 2) { 
+            System.out.print(args[i] + " ");
         }
+
+        System.out.println();  
     }
 }
